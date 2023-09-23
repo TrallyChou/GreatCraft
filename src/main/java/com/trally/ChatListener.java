e86cf8a3b8482a0be80c570a580738a901d5f172
T.l.y <1940003473@qq.com>
2023-09-23 12:39:50 +0800

    1

--- src/main/java/com/trally/ChatListener.java
+++ src/main/java/com/trally/ChatListener.java
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (c) 2023. Trally Chou (Zhou jiale, in Chinese)
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.trally;
 
 import org.bukkit.entity.Player;
